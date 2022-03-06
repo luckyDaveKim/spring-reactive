e46193a616f37cb720962f9ee9ca86c3930972ea
Dave <lucky_dave@naver.com>
2022-03-06 14:14:25 +0900

    feat: Add mapPub

--- src/main/java/com/dave/springreactive/section4/PubSub.java
+++ src/main/java/com/dave/springreactive/section4/PubSub.java
@@ -6,17 +6,19 @@ import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
 
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /*
- * pub -> [Data1] -> logSub
+ * pub -> [Data1] -> mapPub -> [Data2] -> logSub
  * */
 @Slf4j
 public class PubSub {
   public static void main(String[] args) {
     Publisher<Integer> pub = getPub(Stream.iterate(1, i -> i + 1).limit(10).collect(Collectors.toList()));
-    pub.subscribe(getLogSub());
+    Publisher<Integer> mapPub = mapPub(pub, i -> i * 10);
+    mapPub.subscribe(getLogSub());
   }
 
   private static Publisher<Integer> getPub(final List<Integer> iter) {
@@ -34,6 +36,30 @@ public class PubSub {
       @Override
       public void cancel() {
 
+      }
+    });
+  }
+
+  private static Publisher<Integer> mapPub(Publisher<Integer> pub, Function<Integer, Integer> fun) {
+    return sub -> pub.subscribe(new Subscriber<>() {
+      @Override
+      public void onSubscribe(Subscription innerSub) {
+        sub.onSubscribe(innerSub);
+      }
+
+      @Override
+      public void onNext(Integer i) {
+        sub.onNext(fun.apply(i));
+      }
+
+      @Override
+      public void onError(Throwable t) {
+        sub.onError(t);
+      }
+
+      @Override
+      public void onComplete() {
+        sub.onComplete();
       }
     });
   }
