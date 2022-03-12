e06418a8d0d996d16f4a24a809c9fb7bf87af8e4
Dave <lucky_dave@naver.com>
2022-03-12 20:43:08 +0900

    feat: Create futureTask sample

--- src/main/java/com/dave/springreactive/section12/Main.java
+++ src/main/java/com/dave/springreactive/section12/Main.java
@@ -5,20 +5,20 @@ import lombok.extern.slf4j.Slf4j;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
 
 @Slf4j
 public class Main {
   public static void main(String[] args) throws ExecutionException, InterruptedException {
-    ExecutorService es = Executors.newCachedThreadPool();
-
-    // execute는 runnable 인터페이스를 받는다.
-    // submit은 runnable, callable 인터페이스를 받는다. callable은 리턴값이 존재하며, 예외도 던질 수 있다.
-    Future<String> f = es.submit(() -> {
+    FutureTask<String> f = new FutureTask<>(() -> {
       Thread.sleep(2000);
       log.info("Async");
       return "Hello";
     });
+
+    ExecutorService es = Executors.newCachedThreadPool();
+    es.execute(f);
+    es.shutdown();
 
     log.info("isDone : {}", f.isDone());
 
