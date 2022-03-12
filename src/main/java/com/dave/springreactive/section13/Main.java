99f22bd16235844624ee32f63795509aa9c9bf3c
Dave <lucky_dave@naver.com>
2022-03-12 22:40:41 +0900

    feat: Set threadPool info

--- src/main/java/com/dave/springreactive/section13/Main.java
+++ src/main/java/com/dave/springreactive/section13/Main.java
@@ -3,12 +3,24 @@ package com.dave.springreactive.section13;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @EnableAsync
 @SpringBootApplication
 public class Main {
   public static void main(String[] args) {
     ConfigurableApplicationContext ctx = SpringApplication.run(Main.class, args);
+  }
+
+  @Bean
+  ThreadPoolTaskExecutor threadPool() {
+    ThreadPoolTaskExecutor threadPool = new ThreadPoolTaskExecutor();
+    threadPool.setCorePoolSize(10);
+    threadPool.setMaxPoolSize(100);
+    threadPool.setQueueCapacity(200);
+    threadPool.initialize();
+    return threadPool;
   }
 }
