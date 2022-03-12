10b6f31224801d71ee53525785f93f4d5c5f7fbe
Dave <lucky_dave@naver.com>
2022-03-12 20:32:49 +0900

    feat: Create future sample

--- src/main/java/com/dave/springreactive/section12/Main.java
+++ src/main/java/com/dave/springreactive/section12/Main.java
@@ -2,22 +2,30 @@ package com.dave.springreactive.section12;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 @Slf4j
 public class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws ExecutionException, InterruptedException {
     ExecutorService es = Executors.newCachedThreadPool();
 
     // execute는 runnable 인터페이스를 받는다.
     // submit은 runnable, callable 인터페이스를 받는다. callable은 리턴값이 존재하며, 예외도 던질 수 있다.
-    es.submit(() -> {
+    Future<String> f = es.submit(() -> {
       Thread.sleep(2000);
       log.info("Async");
       return "Hello";
     });
 
+    log.info("Future : {}", f.get());
+
     log.info("Exit");
+
+    /*
+    * Async -> Future -> Exit
+    * */
   }
 }
