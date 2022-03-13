6571311ac16878b69c2f83038e90b12b80c41e5e
Dave <lucky_dave@naver.com>
2022-03-13 15:24:06 +0900

    feat: Create SpringBoot async example

---
+++ src/main/java/com/dave/springreactive/section14/Main.java
@@ -0,0 +1,14 @@
+package com.dave.springreactive.section14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@EnableAsync
+@SpringBootApplication
+public class Main {
+  public static void main(String[] args) {
+    ConfigurableApplicationContext ctx = SpringApplication.run(Main.class, args);
+  }
+}
---
+++ src/main/java/com/dave/springreactive/section14/MyController.java
@@ -0,0 +1,16 @@
+package com.dave.springreactive.section14;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class MyController {
+  @GetMapping("/async")
+  public String async() throws InterruptedException {
+    log.info("async()");
+    Thread.sleep(2000);
+    return "hello";
+  }
+}
